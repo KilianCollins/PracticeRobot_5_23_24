5ec5350188c6e20d6e6616ffba96fe9c618379a3
Kilian Collins <134647067+KilianCollins@users.noreply.github.com>
2024-05-31 20:08:31 -0500

    5_31_24-This is the Autonoumous for Blackout Centerstage-Andrew Pookie
    merged project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedSide2BetterNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedSide2BetterNew.java
@@ -1,40 +1,8 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
@@ -56,9 +24,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="RedsideAuto", group="Robot")
+@Autonomous(name="Red Auto2", group="Robot")
 
-public class RedSideParkingAuto extends LinearOpMode {
+public class RedSide2BetterNew extends LinearOpMode {
 
     /* Declare OpMode members. */
     private DcMotor rightFrontDrive = null; // hee hee hee haa change later
@@ -68,16 +36,15 @@ public class RedSideParkingAuto extends LinearOpMode {
     private DcMotor leftFrontDrive = null;
     private DcMotor leftBackDrive = null;
 
-    private DcMotor arm_motor = null;
-    private DcMotor wrist_motor = null;
-    private DcMotor outtake_intake_motor = null;
-    private ElapsedTime runtime = new ElapsedTime();
+    private  DcMotor arm_motor = null;
+    private  DcMotor wrist_motor = null;
+     private DcMotor outtake_intake_motor = null ;
+    private ElapsedTime     runtime = new ElapsedTime();
 
 
-    static final double FORWARD_SPEED = 0.4;
+    static final double     FORWARD_SPEED = 0.4;
     //    static final double     TURN_SPEED    = 0.2;
-    static final double REVERSE_SPEED = -0.4;
-
+    static final double     REVERSE_SPEED = -0.4;
     @Override
     public void runOpMode() {
 
@@ -85,9 +52,9 @@ public class RedSideParkingAuto extends LinearOpMode {
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive ");
         leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
-        arm_motor = hardwareMap.get(DcMotor.class, "arm_motor");
-        wrist_motor = hardwareMap.get(DcMotor.class, "wrist_motor");
+        leftBackDrive = hardwareMap.get(DcMotor.class,"left_back_drive");
+        arm_motor = hardwareMap.get(DcMotor.class,"arm_motor");
+        wrist_motor =hardwareMap.get(DcMotor.class,"wrist_motor");
         outtake_intake_motor = hardwareMap.get(DcMotor.class,"intake");
 
 
@@ -98,10 +65,6 @@ public class RedSideParkingAuto extends LinearOpMode {
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-
-        outtake_intake_motor.setDirection(DcMotor.Direction.REVERSE);
-        arm_motor.setDirection(DcMotor.Direction.REVERSE);
-        wrist_motor.setDirection(DcMotor.Direction.FORWARD);
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "Ready to run");    //
         telemetry.update();
@@ -112,11 +75,34 @@ public class RedSideParkingAuto extends LinearOpMode {
         // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
 
 
+
+
         runtime.reset();
 
         while (opModeIsActive() && (runtime.seconds() < 3.0)) {
             telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
+
+            //Step 1:Strafe Right
+           /* leftFrontDrive.setPower(REVERSE_SPEED);
+            rightFrontDrive.setPower(FORWARD_SPEED);
+            leftBackDrive.setPower(FORWARD_SPEED);
+            rightBackDrive.setPower(REVERSE_SPEED);
+            sleep(1750);
+
+            // Step 2:Go Forward`
+            leftFrontDrive.setPower(REVERSE_SPEED);
+            rightFrontDrive.setPower(REVERSE_SPEED);
+            leftBackDrive.setPower(REVERSE_SPEED);
+            rightBackDrive.setPower(REVERSE_SPEED);
+            sleep(1000);
+
+            leftBackDrive.setPower(0);
+            rightFrontDrive.setPower(0);
+            leftFrontDrive.setPower(0);
+            rightBackDrive.setPower(0);*/
+
+            //Step 3: Arm Up
 
             //Step 1:Strafe Left
             leftFrontDrive.setPower(FORWARD_SPEED);
@@ -139,7 +125,7 @@ public class RedSideParkingAuto extends LinearOpMode {
 
             //Step 3: Arm Up
 
-            arm_motor.setTargetPosition(3350);
+            arm_motor.setTargetPosition(-3350);
             arm_motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             arm_motor.setPower(0.6);
             sleep(2500);//1450 just
@@ -150,10 +136,10 @@ public class RedSideParkingAuto extends LinearOpMode {
             wrist_motor.setTargetPosition(350);
             wrist_motor.setMode((DcMotor.RunMode.RUN_TO_POSITION));
             wrist_motor.setPower(0.6);
-            sleep(2000);
+            sleep(2000); // 2100
 
             //Step 5: outtake and intake drop pixels
-            outtake_intake_motor.setPower(REVERSE_SPEED);
+             outtake_intake_motor.setPower(FORWARD_SPEED);
             sleep(8000);
 //          Step 4: arm and wrist go back to resting position
             arm_motor.setTargetPosition(100);
@@ -167,6 +153,53 @@ public class RedSideParkingAuto extends LinearOpMode {
             sleep(2000);
 
 
+
+
+            //Step 4: Wrist to scoring position
+
+
+            //Step 5: outtake and intake drop pixels
+
+            //outtake_intake_motor.setPower(REVERSE_SPEED);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
 
 
 
@@ -191,7 +224,5 @@ public class RedSideParkingAuto extends LinearOpMode {
 //        telemetry.addData("Path", "Complete");
 //        telemetry.update();
 //        sleep(1000);
-        }
     }
-
 }
